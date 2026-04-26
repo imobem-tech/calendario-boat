98450412bcd93641d19c1ac981a1eab34135e4cf
imobem-tech <imobem@gmail.com>
2026-04-26 20:34:23 -0300

    Update server.js

--- wpp/server.js
+++ wpp/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 8080
 
 
 app.use(express.json())
-app.use("/", retornoRoutes);
+app.use("/msg_externa", retornoRoutes);
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL || process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
