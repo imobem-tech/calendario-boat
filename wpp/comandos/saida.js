f4ba6d6bd84a67d01ebe494dac404ebe3821d9c9
imobem-tech <imobem@gmail.com>
2026-05-26 09:11:34 -0300

    Update saida.js

--- wpp/comandos/saida.js
+++ wpp/comandos/saida.js
@@ -695,3 +695,4 @@ export async function tratarComandoSaida(sock, pool, grupoId, remetente, texto) 
 
   return await iniciarFluxoSaida(sock, pool, grupoId, remetente)
 }
+export { buscarColaborador }
